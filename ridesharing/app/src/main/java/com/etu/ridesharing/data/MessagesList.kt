5a2a2455450403f324632a165e17826e46359683
AntonSeredenkov <seredenkov.anton2003@gmail.com>
2024-03-05 23:26:13 +0300

    MessageList added

---
+++ ridesharing/app/src/main/java/com/etu/ridesharing/data/MessagesList.kt
@@ -0,0 +1,22 @@
+package com.etu.ridesharing.data
+
+object MessagesList {
+    val MessageList = mutableListOf<Message>(
+        Message(
+            author = "",
+            message = "Как открыть техническую поддержку?",
+        ),
+        Message(
+            author = "Техническая поддержка",
+            message = "Откройте меню и перейдите в 'Поддержка'",
+        ),
+        Message(
+            author = "",
+            message = "А если я мне лень заходить?",
+        ),
+        Message(
+            author = "Техническая поддержка",
+            message = "Это Ваша проблема",
+        ),
+    )
+}
