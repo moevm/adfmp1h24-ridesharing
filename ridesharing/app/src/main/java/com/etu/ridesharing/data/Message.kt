bbe83a9a8beee8be734797fc6e2da0199d866e19
AntonSeredenkov <seredenkov.anton2003@gmail.com>
2024-03-18 18:27:32 +0300

    Отображение даты и времени в диалоге

--- ridesharing/app/src/main/java/com/etu/ridesharing/data/Message.kt
+++ ridesharing/app/src/main/java/com/etu/ridesharing/data/Message.kt
@@ -3,5 +3,6 @@ package com.etu.ridesharing.data
 data class Message(
     val author: String = "",
     val message: String = "",
+    val date: String = "",
     val time: String = ""
 )
--- ridesharing/app/src/main/java/com/etu/ridesharing/data/MessagesList.kt
+++ ridesharing/app/src/main/java/com/etu/ridesharing/data/MessagesList.kt
@@ -5,18 +5,26 @@ object MessagesList {
         Message(
             author = "",
             message = "Как открыть техническую поддержку?",
+            date = "20.03.2024",
+            time = "12:00"
         ),
         Message(
             author = "Техническая поддержка",
             message = "Откройте меню и перейдите в 'Поддержка'",
+            date = "20.03.2024",
+            time = "12:05"
         ),
         Message(
             author = "",
             message = "А если я мне лень заходить?",
+            date = "20.03.2024",
+            time = "12:06"
         ),
         Message(
             author = "Техническая поддержка",
             message = "Это Ваша проблема",
+            date = "20.03.2024",
+            time = "12:10"
         ),
     )
 }
